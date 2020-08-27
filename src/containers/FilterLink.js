799ad8469d9fc41cc0f353adfc411f3a6d11663f
Trieu Bui <vantrieu.ou@gmail.com>
2020-08-27 16:52:39 +0700

    reparing react

---
+++ src/containers/FilterLink.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux'
+import { setVisibilityFilter } from '../actions'
+import Link from '../components/Link'
+
+export default connect(
+  (state, ownProps) => ({
+    active: ownProps.filter === state.visibilityFilter,
+  }),
+  (dispatch, ownProps) => ({
+    onClick: () => dispatch(setVisibilityFilter(ownProps.filter)),
+  })
+)(Link)
